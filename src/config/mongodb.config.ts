fbdd7d2f331f356f80931219fad89534a63ad59d
AminIdeaUsher <amin@ideausher.com>
2024-06-29 19:53:21 +0530

    mongod url

--- src/config/mongodb.config.ts
+++ src/config/mongodb.config.ts
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
 
+import config from './env.config';
+
 export default class MongoDB {
   static async connect() {
-    await mongoose.connect('');
+    await mongoose.connect(config.mongoose.url);
     console.log(`DB connection successful!`);
   }
 }
